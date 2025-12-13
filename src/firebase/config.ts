e5f484131b2c3e8fd4df0b62093f02223be9200a
Tatiana <callmetatia@gmail.com>
2025-12-13 07:33:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-524540891-d2d3a",
+  "appId": "1:969466606366:web:7492c388fc068936420f0f",
+  "apiKey": "AIzaSyB9Og9D7g_WxCyKTLMcECsieQDcV0fVlb4",
+  "authDomain": "studio-524540891-d2d3a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "969466606366"
+};
